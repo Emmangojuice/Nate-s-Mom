8fc90bd18847e2871799597a57a4077573172a46
Jorge A <Jarrazola1a@gmail.com>
2024-09-10 17:53:16 -0500

    Jorge's commit maybe
    i dont know what im doing

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ try:
     print("odd" if num % 2 else "even")
 except ValueError:
     print("Input must be a number.")
+
+
+#JA Commiting to Nate's Mom
+#Great presentation, never used github before
